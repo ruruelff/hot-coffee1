e8a143988e3161010fe420d79cdebd4fea18b45e
aradilkha <aruzhanadilkhasym@mail.ru>
2025-08-21 12:46:00 +0500

    repositories

---
+++ internal/dal/utils/repositories.go
@@ -0,0 +1,18 @@
+package repositories
+
+import "hot-coffee/models"
+
+type InventoryRepository interface {
+	ReadInventory() ([]models.InventoryItem, error)
+	WriteInventory([]models.InventoryItem) error
+}
+
+type MenuRepository interface {
+	ReadMenu() ([]models.MenuItem, error)
+	WriteMenu([]models.MenuItem) error
+}
+
+type OrderRepository interface {
+	ReadOrder() ([]models.Order, error)
+	WriteOrder([]models.Order) error
+}
