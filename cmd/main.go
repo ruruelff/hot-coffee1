68c5de19847caa9255ce8ba37c97cded61a46f55
aradilkha <aruzhanadilkhasym@mail.ru>
2025-08-21 12:36:56 +0500

    feat: add main entrypoint for Hot-Coffee server

---
+++ cmd/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"hot-coffee/internal/config"
+	"hot-coffee/internal/handler"
+	"log"
+	"net/http"
+)
+
+func main() {
+	if err := config.ConfigLoad(); err != nil {
+		log.Fatal(err)
+	}
+
+	port := config.GetConfigPort()
+
+	mux := http.NewServeMux()
+
+	handler.InventoryEndpoints(mux)
+	handler.MenuEndpoints(mux)
+	handler.OrderEndpoints(mux)
+	handler.AggregationEndpoints(mux)
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		handler.ErrorResponse(w, "405 - No such method", http.StatusMethodNotAllowed)
+	})
+
+	fmt.Println("Server started listening on port -", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), mux))
+}
